30e14b939a91bc6ebeb89d01b0f9ff0c6910dc07
yassinmftah <yassinmaftah06@gmail.com>
2026-01-05 14:26:54 +0100

    add method getAllPostUser and InsetToArrayPost and change logic of createPost use UserRepo

--- classes/BasicUser.php
+++ classes/BasicUser.php
@@ -1,12 +1,14 @@
 <?php
 require_once 'User.php';
 require_once __DIR__ . '/../config/Database.php';
+require_once __DIR__ . '/../repo/UseRepo.php';
 
 class BasicUser extends User {
     
     private int $monthly_uploads;
+    private array $PostUser;
 
-    public function __construct(string $username, string $email, string $password, string $role, ?int $id = null, ?string $bio = null,?string $profile_picture = null,
+    public function __construct(string $username, string $email, string $password, string $role,$PostUser = [], ?int $id = null, ?string $bio = null,?string $profile_picture = null,
     bool $is_active = true,?DateTime $created_at = null,?DateTime $last_login = null,int $monthly_uploads = 0 ) 
     {
         parent::__construct(
@@ -14,19 +16,32 @@ class BasicUser extends User {
             $profile_picture, $is_active, $created_at, $last_login
         );
         $this->monthly_uploads = $monthly_uploads;
+        $this->PostUser = $PostUser;
     }
 
-    public function createPost(Post $post) {
-        $db = Database::getConnection();
+    public function getAllPostUser() { return $this-> PostUser;}
+    public function InsetToArrayPost(array $allpost) { $this->PostUser = $allpost; }
 
-        $sql = "INSERT INTO posts (user_id, title, description, file_path, file_size, mime_type, dimensions, created_at) 
-                VALUES (?, ?, ?, ?, ?, ?, ?, NOW())";
+    // public function createPost(Post $post) {
+    //     $db = Database::getConnection();
+
+    //     $sql = "INSERT INTO posts (user_id, title, description, file_path, file_size, mime_type, dimensions, created_at) 
+    //             VALUES (?, ?, ?, ?, ?, ?, ?, NOW())";
         
-        $stmt = $db->prepare($sql);
+    //     $stmt = $db->prepare($sql);
 
-        return $stmt->execute([$post->getUserId(),$post->getTitle(),
-            $post->getDescription(),$post->getFilePath(),$post->getFileSize(),
-            $post->getMimeType(),$post->getDimensions()]);
+    //     return $stmt->execute([$post->getUserId(),$post->getTitle(),
+    //         $post->getDescription(),$post->getFilePath(),$post->getFileSize(),
+    //         $post->getMimeType(),$post->getDimensions()]);
+    // }
+
+    public function createPost(Post $post) 
+    {
+        $UserRepo = new UserRepo();
+
+        $newpost = $UserRepo->InsertPost($post);
+        $allPost = $UserRepo->getPosts($this->getId());
+        $this->InsetToArrayPost($allPost);
     }
 
     public function addComment(Comment $comm) 
