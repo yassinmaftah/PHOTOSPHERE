6d175d5a2eae776b7b6a0704fc30109cce3c37ad
yassinmftah <yassinmaftah06@gmail.com>
2026-01-06 12:49:58 +0100

    New File Uses for class CommentRepo , create methods to take and insert data from Table comments in photosphere databse

---
+++ repo/CommentRepo.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once __DIR__ . '/../config/Database.php';
+require_once __DIR__ . '/../classes/Comment.php';
+
+class CommentRepo
+{
+    public function AddComment (Comment $com)
+    {
+        $db = Database::getConnection();
+        $sql = "INSERT INTO comments (user_id, post_id, content) VALUES (?,?,?)";
+        $stmt = $db->prepare($sql);
+        
+        if ($stmt->execute([$com->getUserId(),$com->getPostId(),$com->getContent()]))
+            return true;
+        return false;
+    }
+
+    public function DeleteComment($ComId)
+    {
+        $db = Database::getConnection();
+        $sql = "DELETE FROM Comments WHERE id = ?";
+        $stmt = $db->prepare($sql);
+        echo $ComId;
+        if ($stmt->execute([$ComId]));
+            return true;
+        return false;
+    }
+
+    public function getCommentsByPostId($postId):array {
+        $db = Database::getConnection();
+        $AllComments = [];
+        $sql = "SELECT comments.*, users.username 
+                FROM comments 
+                JOIN users ON comments.user_id = users.id 
+                WHERE post_id = ? 
+                ORDER BY created_at ASC";
+        
+        $stmt = $db->prepare($sql);
+        $stmt->execute([$postId]);
+        $CommentArray = $stmt->fetchAll();
+        // int $user_id, int $post_id, string $content,
+        foreach($CommentArray as $comment)
+        {
+            $comment = new Comment($comment['user_id'],$comment['post_id'],$comment['content']);
+            $AllComments[] = $comment;
+        }
+        return $AllComments;
+    }
+
+}
