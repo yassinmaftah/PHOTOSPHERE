63813c9a04f4869aee4916b21bcabc7aae2a3cba
yassinmftah <yassinmaftah06@gmail.com>
2026-01-06 12:46:55 +0100

    Create new methods CRUD Comments

--- classes/Comment.php
+++ classes/Comment.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../config/Database.php';
+require_once __DIR__ . '/../repo/CommentRepo.php';
 
 class Comment {
     private ?int $id;
@@ -21,5 +22,40 @@ class Comment {
     public function getPostId(){return $this->post_id;}
     public function getContent(){return $this->content;}
     public function getCreatedAt() {return $this->created_at;}
+
+    public static function NewCommentData($userID, $postID, $Content)
+    {
+        $repo = new CommentRepo();
+
+        $com = new Comment($userID, $postID, $Content);
+        if ($repo->AddComment($com))
+            echo "Comment Added";
+        else
+            echo "Comment NOT Added";
+    }
+
+    public static function DeleteCom($idComment)
+    {
+        echo $idComment;
+        $repo = new CommentRepo();
+        if ($repo->DeleteComment($idComment))
+            echo "Comment with id : " . $idComment . "Deleted";
+        else
+            echo "Comment with id : " . $idComment . "Not Deleted";
+    }
+
+    public static function CommentPost($postID)
+    {
+        $repo = new CommentRepo();
+        $Comments = $repo->getCommentsByPostId($postID);
+        // var_dump($Comments);
+        foreach($Comments as $comment)
+        {
+            echo "id Comment : " . $comment->getId() . "<br>";
+            echo "Use ID : " . $comment->getUserId() . "<br>";
+            echo "Post ID : " . $comment->getPostId() . "<br>";
+            echo "Content : " . $comment->getContent() . "<br>";
+        }
+    }
 }
 ?>
